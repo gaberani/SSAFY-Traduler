b5bf64249e8b5e4562d04817076b5b1b0af8972f
gaberani <khs0783@naver.com>
2020-10-26 14:27:16 +0900

    [HS] develop-front / DELETE about.vue, spot.vue path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,24 +7,17 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/spot',
-    name: 'Spot',
-    component: () => import(/* webpackChunkName: "spot" */ '../views/Spot.vue')
-  },
+  // {
+  //   path: '/spot',
+  //   name: 'Spot',
+  //   component: () => import(/* webpackChunkName: "spot" */ '../views/Spot.vue')
+  // },
   {
     path: '/',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
 ]
 
 const router = new VueRouter({
