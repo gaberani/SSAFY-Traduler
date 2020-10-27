ab87335cce977f930ae9d5d925140a28d46ac767
unknown <chriskwon96@naver.com>
2020-10-27 14:55:51 +0900

    [KE] spot views.py / ADD retrieve

--- backend/Traduler/spots/views.py
+++ backend/Traduler/spots/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Avg
 
 from rest_framework import viewsets
 from rest_framework.decorators import permission_classes, action
 from rest_framework.response import Response
 
-from .models import Spot, Category, Area
-from .serializer import SpotSerializer, CategorySerializer, AreaSerializer
+from .models import Spot, Category, Area, SpotComment
+from .serializer import SpotSerializer, CategorySerializer, AreaSerializer, SpotCommentSerializer
 
 # Create your views here.
 class AreaViewSet(viewsets.ModelViewSet):
@@ -18,10 +19,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
 
 
-
 class SpotViewSet(viewsets.ModelViewSet):
     queryset = Spot.objects.all()
     serializer_class = SpotSerializer
+    comment_queryset = SpotComment.objects.all()
+    comment_serializer = SpotCommentSerializer
 
     def list(self, request, *args, **kwargs):
         title = request.GET.get('title', None)
@@ -35,9 +37,20 @@ class SpotViewSet(viewsets.ModelViewSet):
         if area_code:
             filtered_spots = filtered_spots.filter(area_code__area_code__startswith=area_code)
 
-        spot_serializer = self.serializer_class(filtered_spots, many=True)
+        serialized_spots = self.serializer_class(filtered_spots, many=True)
 
-        return Response(spot_serializer.data)
+        return Response(serialized_spots.data)
+
+    def retrieve(self, request, pk):
+        spot = self.queryset.get(id=pk)
+        serialized_spot = self.serializer_class(spot)
+
+        filtered_comments = self.comment_queryset.filter(spot_pk=pk)
+        serialized_comments = self.comment_serializer(filtered_comments, many=True)
+
+        average_score = filtered_comments.aggregate(Avg('score'))
+
+        return Response({"spot": serialized_spot.data, "comments":serialized_comments.data, "score": average_score})
 
 
 
