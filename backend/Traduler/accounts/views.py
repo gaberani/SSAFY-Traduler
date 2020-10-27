02a0159c7ba16f5d9ff913625f3ae3804976a876
unknown <chriskwon96@naver.com>
2020-10-27 14:25:41 +0900

    [KE] spot model / ADD Spot models.py

--- backend/Traduler/accounts/views.py
+++ backend/Traduler/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import permission_classes, action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
