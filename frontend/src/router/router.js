514bb80ffcfd89ca4dae331d5a8847eccb50f6e4
gaberani <khs0783@naver.com>
2020-10-27 14:43:06 +0900

    [HS] develop-front / ADD router.js

---
+++ frontend/src/router/router.js
@@ -0,0 +1,14 @@
+export default {
+  URL_TYPE: {
+		SCHEDULE: {
+			
+		},
+		USER: {
+			SIGNUP: 'rest-auth/signup',
+			LOGIN: 'rest-auth/login',
+			LOGOUT: 'rest-auth/logout',
+			MYPAGE: 'mypage',
+			MYINFOEDIT: 'accounts', // PATCH: 수정, DELETE: 삭제
+		}
+	}
+}
