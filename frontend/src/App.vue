f218f628076c85f387dfc264c38fb97655e7d99e
gaberani <khs0783@naver.com>
2020-10-27 14:34:51 +0900

    [HS] develop-front / TEST jalnan font test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
     <Header/>
+    <h1>Lorem Ipsum Dolor</h1>
+    <h3>ㅎㅎㅎㅎㅎ</h3>
+    <h2>강원도는 어때요?</h2>
     <router-view :key="$route.fullPath"></router-view>
   </v-app>
 </template>
@@ -19,3 +22,15 @@ export default {
   }),
 };
 </script>
+
+<style>
+@import url(./fonts/jalnan/stylesheet.css);
+
+html, body {
+  font-family:'jalnanregular';
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'jalnanregular';
+}
+</style>
