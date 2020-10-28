b27f7297295c8b382923d3376ba0705ae33fcf7f
unknown <chriskwon96@naver.com>
2020-10-28 15:14:40 +0900

    [KE] spot serializer / FIX SpotCommentSerializer + user

--- backend/Traduler/spots/serializer.py
+++ backend/Traduler/spots/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import Spot, Category, Area, SpotComment, CustomSpot
+from accounts.serializers import UserSerializer
 
 
 class SpotSerializer(serializers.ModelSerializer):
@@ -20,9 +21,11 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class SpotCommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(required=False)
     class Meta:
         model = SpotComment
         fields = "__all__"
+        read_only_fields = ('reg_time', 'user_pk')
 
 class CustomSpotSerializer(serializers.ModelSerializer):
     class Meta:
