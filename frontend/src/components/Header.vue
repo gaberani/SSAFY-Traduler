69dbcd78fbc4f86de36559f5ae7635159d99988d
gaberani <khs0783@naver.com>
2020-10-28 10:58:42 +0900

    [HS] Header title / ADD logo title font style

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -6,7 +6,7 @@
   >
     <v-toolbar id="navb">
       <img  style="height:70%;" src="@/assets/tradulerlogo.png" />
-      <v-toolbar-title>Traduler</v-toolbar-title>
+      <v-toolbar-title style="font-family: 'jalnanregular'">Traduler</v-toolbar-title>
       <v-spacer ></v-spacer>
       <v-btn text>홈</v-btn>     
       <v-btn text>스케쥴러</v-btn>    
@@ -32,5 +32,13 @@ export default {
 </script>
 
 <style>
+@import url(../fonts/jalnan/stylesheet.css);
 
+html, body {
+  font-family:'jalnanregular';
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'jalnanregular';
+}
 </style>
