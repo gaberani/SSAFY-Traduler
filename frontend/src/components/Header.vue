714e7ac98e1b0d487b35f66ebf0b5f51da20deec
gaberani <khs0783@naver.com>
2020-10-28 09:27:20 +0900

    [HS] components Header.vue / ADD navbar btn event

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,14 +5,13 @@
     tile
   >
     <v-toolbar id="navb">
-        <img  style="height:70%;" src="@/assets/tradulerlogo.png" />
+      <img  style="height:70%;" src="@/assets/tradulerlogo.png" />
       <v-toolbar-title>Traduler</v-toolbar-title>
       <v-spacer ></v-spacer>
-        <v-btn text>홈</v-btn>     
-        <v-btn text>스케쥴러</v-btn>    
-        <v-btn text @click="gotoSpot">여행지 검색</v-btn>     
-        <v-btn text>로그인</v-btn>     
-        
+      <v-btn text>홈</v-btn>     
+      <v-btn text>스케쥴러</v-btn>    
+      <v-btn text @click="gotoSpot">여행지 검색</v-btn>     
+      <v-btn text @click="gotoLogin">로그인</v-btn>     
     </v-toolbar>
   </v-card>
 </template>
@@ -22,9 +21,12 @@
 export default {
     name: 'Header',
     methods: {
-        gotoSpot() {
-            this.$router.push("/spot")
-        },
+      gotoSpot() {
+        this.$router.push("/spot")
+      },
+      gotoLogin() {
+        this.$router.push({name:"Login"})
+      }
     }
 }
 </script>
