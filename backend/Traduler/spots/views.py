41431cfee5d578531114fd1d604b00bd7d31f8f2
unknown <chriskwon96@naver.com>
2020-10-29 10:08:47 +0900

    [KE] spots views.py/ ADD BasicCRUDPermission to CustomSpot

--- backend/Traduler/spots/views.py
+++ backend/Traduler/spots/views.py
@@ -86,7 +86,15 @@ class CustomSpotViewSet(viewsets.ModelViewSet):
     queryset = CustomSpot.objects.all()
     serializer_class = CustomSpotSerializer
 
-    permission_classes=[]
+    permission_classes=[BasicCRUDPersmisson]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user_pk=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 
 class SpotCommentViewSet(viewsets.ModelViewSet):
