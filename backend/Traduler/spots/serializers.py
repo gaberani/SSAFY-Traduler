22b97d27eb961a586f9da26cc45561d1f6ff65f0
unknown <chriskwon96@naver.com>
2020-10-29 14:35:29 +0900

    [KE] SpotSerializer / ADD get_is_liked

--- backend/Traduler/spots/serializers.py
+++ backend/Traduler/spots/serializers.py
@@ -5,9 +5,21 @@ from accounts.serializers import UserSerializer
 
 
 class SpotSerializer(serializers.ModelSerializer):
+    is_liked = serializers.SerializerMethodField()
+
     class Meta:
         model = Spot
         fields = "__all__"
+
+    def get_is_liked(self, obj):
+        # 현재 유저가 로그인한 사용자인 경우입니당
+        if self.context.get('user', False) and self.context['user'].is_authenticated:
+            temp_val = obj.liked.all().filter(user_pk=self.context['user']).exists()
+        # 로그인 안 한 사용자는 모두 False로 리턴합니다.
+        else:
+            temp_val = False
+
+        return temp_val
 
 
 class AreaSerializer(serializers.ModelSerializer):
@@ -21,14 +33,14 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class SpotCommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(required=False)
+    user = UserSerializer(source='user_pk', required=False)
     class Meta:
         model = SpotComment
         fields = "__all__"
         read_only_fields = ('reg_time', 'user_pk')
 
 class CustomSpotSerializer(serializers.ModelSerializer):
-    user = UserSerializer(required=False)
+    user = UserSerializer(source='user_pk', required=False)
     class Meta:
         model = CustomSpot
         fields = "__all__"
