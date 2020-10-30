0c0821a673ba5742695c617f4c50c04d802886dc
gaberani <khs0783@naver.com>
2020-10-30 13:34:34 +0900

    [HS] Header.vue / ADD logout, loginflag

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -8,20 +8,23 @@
         <img  style="height:70%;" src="@/assets/tradulerlogo.png" />
       <v-toolbar-title style="font-family: 'jalnanregular'; font-size:1.5rem">Traduler</v-toolbar-title>
       <v-spacer ></v-spacer>
-        <v-btn text @click="gotoHome" style="font-family: 'SCDream5'">홈</v-btn>     
-        <v-btn text style="font-family: 'SCDream5'">스케쥴러</v-btn>    
-        <v-btn text @click="gotoSpot" style="font-family: 'SCDream5'">여행지 검색</v-btn>     
-        <v-btn text @click="gotoLogin" style="font-family: 'SCDream5'">로그인</v-btn>     
-        
+        <v-btn text @click="gotoHome" class="navbar-btn">홈</v-btn>     
+        <v-btn text class="navbar-btn">스케쥴러</v-btn>    
+        <v-btn text @click="gotoSpot" class="navbar-btn">여행지 검색</v-btn>     
+        <v-btn v-if="!this.LoginFlag" text @click="gotoLogin" class="navbar-btn">로그인</v-btn>
+        <v-btn v-else text @click="SubmitLogout" class="navbar-btn">로그아웃</v-btn>
     </v-toolbar>
   </v-card>
 </template>
 
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
     name: 'Header',
     methods: {
+      ...mapActions(["SubmitLogout"]),
       gotoSpot() {
         this.$router.push("/spot")
       },
@@ -31,6 +34,9 @@ export default {
       gotoHome() {
         this.$router.push({name:"Home"})
       }
+    },
+    computed: {
+      ...mapGetters(['LoginFlag'])
     }
 }
 </script>
@@ -41,8 +47,10 @@ export default {
 html, body {
   font-family:'jalnanregular';
 }
-
 h1, h2, h3, h4, h5, h6 {
   font-family: 'jalnanregular';
+}
+.navbar-btn {
+  font-family: 'SCDream5'
 }
 </style>
