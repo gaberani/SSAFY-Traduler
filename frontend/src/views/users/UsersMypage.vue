a4969d4ce9a4cb0841b9550437e6d771d336cad1
gaberani <khs0783@naver.com>
2020-11-02 15:32:26 +0900

    [HS] Mypage / ADD grid

--- frontend/src/views/users/UsersMypage.vue
+++ frontend/src/views/users/UsersMypage.vue
@@ -1,14 +1,35 @@
 <template>
+	<v-container>
 		<v-row>
-			<v-col>
-				<h1>내 정보</h1>
-				<v-btn
-					@click="GetUserInfo"
-				>
-					내 정보 확인
-				</v-btn>
-			</v-col>
+			<v-col cols="2" md="3"></v-col>
+			<!-- 회원 정보 -->
+				<v-row>
+					<v-col>
+						<h1>내 정보</h1>
+						<v-btn
+							@click="GetUserInfo"
+						>
+							내 정보 확인
+						</v-btn>
+					</v-col>
+				</v-row>
+
+				<v-row>
+					<!-- 내 스케줄 -->
+
+					<!-- 스크랩한 스케줄 -->
+
+				</v-row>
+
+				<v-row>
+					<v-col></v-col>
+					<!-- 동행 스케줄 -->
+
+					<!-- 즐겨찾기한 여행지 -->
+				</v-row>
+			<v-col cols="2" md="3"></v-col>
 		</v-row>
+	</v-container>
 </template>
 
 <script>
