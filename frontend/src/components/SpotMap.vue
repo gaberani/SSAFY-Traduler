7ad287e6e92f83d35f09591020a45818c353f7d1
gaberani <khs0783@naver.com>
2020-11-05 13:44:25 +0900

    [HS] SpotMap.vue / ADD scroll, move block

--- frontend/src/components/SpotMap.vue
+++ frontend/src/components/SpotMap.vue
@@ -32,34 +32,36 @@ export default {
       var MapContainer = document.getElementById('map' + this.item);
       var MapOption = {
         // 지도의 좌표
-        // center: new kakao.maps.LatLng(33.450701, 126.570667),
         center: new kakao.maps.LatLng(37.501239, 127.039652),
         // 지도의 레벨(확대, 축소 정도)
-        level: 4
+        level: 5
       };
       // 지도 생성 및 객체 리턴
       var map = new kakao.maps.Map(MapContainer, MapOption);
       // 정적인 지도
-      // var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-      // staticMap.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+      // var map = new kakao.maps.StaticMap(MapContainer, MapOption);
 
       // 여행지 간 좌표로 그려질 선 객체
       var linePath = [
-        new kakao.maps.LatLng(37.50044353828887, 127.03718408677545),
-        new kakao.maps.LatLng(37.50128969715334, 127.03960466576542),
-        new kakao.maps.LatLng(37.50127800000000, 127.04000000000000)
+        new kakao.maps.LatLng(37.50044353828887, 127.03718466576542),
+        new kakao.maps.LatLng(37.50127800000000, 127.04000408677545),
+        new kakao.maps.LatLng(37.50128969715334, 127.03960000000000)
       ];
       // var distanceOverlay; // 선의 거리정보를 표시할 커스텀오버레이 입니다
       // var dots = {}; // 여행지 마커 커스텀 오버레이 배열입니다.
 
       var polyline = new kakao.maps.Polyline({
-        path: linePath, // 선을 구성하는 좌표배열 입니다
-        strokeWeight: 5, // 선의 두께 입니다
+        path: linePath,         // 선을 구성하는 좌표배열 입니다
+        strokeWeight: 5,        // 선의 두께 입니다
         strokeColor: '#000000', // 선의 색깔입니다
-        strokeOpacity: 0.7, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-        strokeStyle: 'solid' // 선의 스타일입니다
+        strokeOpacity: 0.7,     // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+        strokeStyle: 'solid'    // 선의 스타일입니다
       });
       polyline.setMap(map)
+
+      // 드래그, 확대축소 막기
+      map.setDraggable(false);
+      map.setZoomable(false);
 
       // 지도의 타입 설정
       map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
