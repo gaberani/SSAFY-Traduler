35501aff537bec73fe430a5453c33c6e229f7590
강찬엽 <kang941194@nate.com>
2020-11-11 12:36:01 +0900

    [CY] front-schedulemain / Fix detail schedule map design

--- frontend/src/components/SpotMap2.vue
+++ frontend/src/components/SpotMap2.vue
@@ -37,6 +37,10 @@ export default {
       document.head.appendChild(script);
     }
 		},
+		// gotodetailspot() {
+		// 	console.log('111111111111111111')
+		// 	// this.$router.push({name:'SpotDetail', params: {spot_id: pk} })
+		// },
     initMap() {
       var MapContainer = document.getElementById('map');
       var MapOption = {
@@ -55,8 +59,15 @@ export default {
 			var Positions = [];
       for (var i=0; i<this.SDdetail.course_coords.length; i++) {
 				linePath.push(new kakao.maps.LatLng(this.SDdetail.course_coords[i][0], this.SDdetail.course_coords[i][1])); 
-				Positions.push({title:"추가할거야~",latlng:new kakao.maps.LatLng(this.SDdetail.course_coords[i][0], this.SDdetail.course_coords[i][1])});   
-        }
+				Positions.push({title:this.SDdetail.course[i].spot_info.title,
+				latlng:new kakao.maps.LatLng(this.SDdetail.course_coords[i][0], this.SDdetail.course_coords[i][1]),
+				address:this.SDdetail.course[i].spot_info.address,
+				image:this.SDdetail.course[i].spot_info.image,
+				id:this.SDdetail.course[i].spot_info.id,
+				course: i+1,
+				});   
+				// title:this.SDdetail.course[i].spot_info.title,
+				}
         // console.log(linePath)
       // var distanceOverlay; // 선의 거리정보를 표시할 커스텀오버레이 입니다
       // var dots = {}; // 여행지 마커 커스텀 오버레이 배열입니다.
@@ -71,11 +82,44 @@ export default {
 			for (var j = 0; j < Positions.length; j ++) { 
     
 			// 마커를 생성합니다
-			var marker = new kakao.maps.Marker({
-					map: map, // 마커를 표시할 지도
-					position: Positions[j].latlng, // 마커를 표시할 위치
-					title : Positions[j].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다 
+				var marker = new kakao.maps.Marker({
+						map: map, // 마커를 표시할 지도
+						position: Positions[j].latlng, // 마커를 표시할 위치
 				});
+				var iwContent='<div style="width:350px; font-family: SCDream5;">' 
+				+ Positions[j].title + '<br>' 
+				+ '<span style="font-size:0.9rem; font-family: SCDream4;">' +Positions[j].address +'</span>' 
+				+ '<img style="width:300px;" src="' +Positions[j].image + '"/>'
+				// + '<button id="test1" style="margin-bottom:5px; display:block; background-color:#FF5E5E; width:100px; border-radius:10px; color:white;">상세보기</button>' 
+				+'</div>'
+				
+				var iwContent2='<div style="width:280px; font-family: SCDream4;">' 
+				+ Positions[j].title 
+				+ '<p style="font-family: SCDream4; margin:0px; color:#707070; font-size:0.8rem;">자세히 보려면 마커를 눌러주세요.</p>' +'</div>' 
+				
+				var infowindow = new kakao.maps.InfoWindow({
+				content: iwContent, // 인포윈도우에 표시할 내용
+				removable :true
+				});
+				var infowindow2 = new kakao.maps.InfoWindow({
+				content: iwContent2, // 인포윈도우에 표시할 내용
+				});
+						kakao.maps.event.addListener(marker, 'click', makeOverListener(map, marker, infowindow));
+						kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow2));
+						// kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+						kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow2));
+			}
+			function makeOverListener(map, marker, infowindow) {
+					return function() {
+							infowindow.open(map, marker);
+					};
+			}
+
+			// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+			function makeOutListener(infowindow) {
+					return function() {
+							infowindow.close();
+					};
 			}
       polyline.setMap(map)
 			marker.setMap(map);
