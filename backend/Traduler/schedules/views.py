30c883a33cc7e83a3a21aa800005c13064b4cdb1
unknown <chriskwon96@naver.com>
2020-11-13 18:13:38 +0900

    [KE] ADD if course exists

--- backend/Traduler/schedules/views.py
+++ backend/Traduler/schedules/views.py
@@ -121,15 +121,18 @@ class ScheduleViewSet(viewsets.ModelViewSet):
             serialized_schedule['coords'] = []
             contained_courses = Course.objects.filter(schedule_pk=serialized_schedule['id']).order_by('start_time')
             sum_lat, sum_lon = 0, 0
-            for contained_course in contained_courses:
-                # 포함된 코스가 spot / custome_spot 2종류이므로 분기해줬습니다...
-                if contained_course.spot_pk:
-                    serialized_schedule['coords'].append([contained_course.spot_pk.lat, contained_course.spot_pk.lon])
-                    sum_lat += contained_course.spot_pk.lat
-                    sum_lon += contained_course.spot_pk.lon
-                else:
-                    serialized_schedule['coords'].append([contained_course.custom_spot_pk.lat, contained_course.custom_spot_pk.lon])
-            serialized_schedule['avg_coord'] = [sum_lat/len(serialized_schedule['coords']), sum_lon/len(serialized_schedule['coords'])]
+            if contained_courses:
+                for contained_course in contained_courses:
+                    # 포함된 코스가 spot / custome_spot 2종류이므로 분기해줬습니다...
+                    if contained_course.spot_pk:
+                        serialized_schedule['coords'].append([contained_course.spot_pk.lat, contained_course.spot_pk.lon])
+                        sum_lat += contained_course.spot_pk.lat
+                        sum_lon += contained_course.spot_pk.lon
+                    else:
+                        serialized_schedule['coords'].append([contained_course.custom_spot_pk.lat, contained_course.custom_spot_pk.lon])
+                serialized_schedule['avg_coord'] = [sum_lat/len(serialized_schedule['coords']), sum_lon/len(serialized_schedule['coords'])]
+            else:
+                serialized_schedule['avg_coord'] = [37.7576, 128.8737]
         
         return Response({"schedule": result}, status=status.HTTP_200_OK)
 
@@ -142,15 +145,18 @@ class ScheduleViewSet(viewsets.ModelViewSet):
             serialized_schedule['coords'] = []
             contained_courses = Course.objects.filter(schedule_pk=serialized_schedule['id']).order_by('start_time')
             sum_lat, sum_lon = 0, 0
-            for contained_course in contained_courses:
-                # 포함된 코스가 spot / custome_spot 2종류이므로 분기해줬습니다...
-                if contained_course.spot_pk:
-                    serialized_schedule['coords'].append([contained_course.spot_pk.lat, contained_course.spot_pk.lon])
-                    sum_lat += contained_course.spot_pk.lat
-                    sum_lon += contained_course.spot_pk.lon
-                else:
-                    serialized_schedule['coords'].append([contained_course.custom_spot_pk.lat, contained_course.custom_spot_pk.lon])
-            serialized_schedule['avg_coord'] = [sum_lat/len(serialized_schedule['coords']), sum_lon/len(serialized_schedule['coords'])]
+            if contained_courses:
+                for contained_course in contained_courses:
+                    # 포함된 코스가 spot / custome_spot 2종류이므로 분기해줬습니다...
+                    if contained_course.spot_pk:
+                        serialized_schedule['coords'].append([contained_course.spot_pk.lat, contained_course.spot_pk.lon])
+                        sum_lat += contained_course.spot_pk.lat
+                        sum_lon += contained_course.spot_pk.lon
+                    else:
+                        serialized_schedule['coords'].append([contained_course.custom_spot_pk.lat, contained_course.custom_spot_pk.lon])
+                serialized_schedule['avg_coord'] = [sum_lat/len(serialized_schedule['coords']), sum_lon/len(serialized_schedule['coords'])]
+            else:
+                serialized_schedule['avg_coord'] = [37.7576, 128.8737]
         
 
         return Response({"schedules": serialized_schedules.data}, status=status.HTTP_200_OK)
@@ -208,7 +214,10 @@ class ScheduleViewSet(viewsets.ModelViewSet):
             course_coords.append((lat, lon))
         
         #소수점 3자리수까지 평균 lat, lon
-        avg_coord = [round(sum_lat/len(course_coords), 4), round(sum_lon/len(course_coords), 4)]
+        if contained_courses:
+            avg_coord = [round(sum_lat/len(course_coords), 4), round(sum_lon/len(course_coords), 4)]
+        else:
+            avg_coord = [37.7576, 128.8737]
 
         # 목적지 정보 가져오기
         contained_provinces = schedule.contained_provinces.all().order_by('start_date')
