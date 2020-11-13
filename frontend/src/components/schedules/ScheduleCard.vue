c62815e062aadc31ad46e85768853f55c05a945a
Donghyeok-Lee <lee33843@gmail.com>
2020-11-13 18:31:10 +0900

    [DH] Scheduld Card / UPDATE together, advice img css

--- frontend/src/components/schedules/ScheduleCard.vue
+++ frontend/src/components/schedules/ScheduleCard.vue
@@ -1,11 +1,13 @@
 <template>
   <v-container style="padding:0;">
-      <!-- v-if -->
-      <img v-if="schedule.together==1" class="togetherimo" src="@/assets/friend.png" alt="">
-      <!-- v-if -->
-      <!-- <img class="helpimo" src="../assets/help.png" alt=""> -->
-      <img v-if="schedule.advice==1" class="helpimo" src="@/assets/help2.png" alt="">
     <SpotMap :data="this.$attrs.data" :schedule="this.$attrs.schedule" class="cardmap"></SpotMap>
+
+    <v-row style="display: flex; justify-content: space-around; margin-top: -4vw; margin-bottom: 1vw;">
+      <img v-if="schedule.together==1" src="@/assets/friend.png" alt="" style="width: 3vw; z-index: 100;">
+      <img v-if="!test" src="@/assets/trans.png" style="width: 3vw; z-index: 100;"/>
+      <img v-if="schedule.advice==1" src="@/assets/help2.png" alt="" style="width: 3vw; z-index: 100;">
+    </v-row>
+
     <button @click="GotoDetail(schedule.id)" class="SDtitle"><span>{{schedule.title}}</span></button>
   </v-container>
 </template>
@@ -21,13 +23,17 @@ export default {
         schedule:[],
       }
     },
+    computed: {
+      test() {
+        return (this.schedule.together * this.schedule.advice)
+      }
+    },
     created() {
-      this.schedule =this.$attrs.schedule
-      // console.log(this.schedule)
+      this.schedule =this.$attrs.schedule;
     },
     methods: {
       GotoDetail(schedule_id) {
-        this.$router.push({name: 'DetailSchedule', params: {schedule_id: schedule_id}})
+        this.$router.push({name: 'DetailSchedule', params: {schedule_id: schedule_id}});
       }
     }
 }
@@ -39,34 +45,16 @@ export default {
     cursor: pointer; 
 }
 .SDtitle {
-    /* background-color:rgba( 255, 19, 19, 0.6 ); */
-    background-color:#FF5E5E;
-    width:100%;
-    height:3vw;
-    font-family: 'SCDream5';
-    color:white;
-    font-size:1.5vw;
-    /* margin-top:-100px; */
-    border-bottom-left-radius: 30px;
+  /* background-color:rgba( 255, 19, 19, 0.6 ); */
+  background-color:#FF5E5E;
+  width:100%;
+  height:3vw;
+  font-family: 'SCDream5';
+  color:white;
+  font-size:1.5vw;
+  /* margin-top:-100px; */
+  border-bottom-left-radius: 30px;
   border-bottom-right-radius: 30px;
 }
-.togetherimo {
-    z-index:2; 
-    /* background-color:#FF9617;  */
-    position:absolute; float:left; 
-    margin-top:14.5%;
-    height:3vw; 
-    width:3%;
-    outline: none;
-    margin-left:1.7%;
-}
-.helpimo {
-    z-index:2; 
-    position:absolute; float:left; 
-    margin-top:14.5%;
-    height:3vw; 
-    width:3%;
-    outline: none;
-    margin-left:16.4%;
-}
+
 </style>
